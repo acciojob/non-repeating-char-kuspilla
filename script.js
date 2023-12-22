2bb4acc1ec1be57128160c3163ce4b15c0d84c48
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-12-22 06:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	 if (str === "") return null;
-            else {
-                for (let i = 0; i < str.length; i++) {
-                    let ans = 0;
-                    for (let j = i + 1; j < str.length; j++) {
-                        if (str[i] === str[j]) {
-                            ans++;
-                        }
-                    }
-                    if (ans === 0) return str[i];
-                }
-                return null;
-	}
+
+    for (let i = 0; i < str.length; i++) {
+        if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+            return str[i];
+        }
+    }
+
+    return null;
+
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
